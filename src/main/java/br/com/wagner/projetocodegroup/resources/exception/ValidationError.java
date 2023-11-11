d01e647b32d9c27fb62456dc8e9b711ae5402f11
Wagner Marques <dba@wm-si.com.br>
2023-11-11 15:57:34 -0300

    Adding handlers for exceptions errors

---
+++ src/main/java/br/com/wagner/projetocodegroup/resources/exception/ValidationError.java
@@ -0,0 +1,25 @@
+package br.com.wagner.projetocodegroup.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Long timeStamp, Integer status, String error, String message, String path) {
+        super(timeStamp, status, error, message, path);
+        // TODO Auto-generated constructor stub
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+
+}
